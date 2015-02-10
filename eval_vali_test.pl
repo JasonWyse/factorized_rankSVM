639aafa4b49aea70a92d1d2b9604f0520035665f
JasonWyse <timeismoneyzyg@163.com>
2015-02-11 00:40:21 +0800

    eval.pl

--- eval_vali_test.pl
+++ eval_vali_test.pl
@@ -6,8 +6,13 @@ my $max_ite_num = $ARGV[0];
 my $dataset_dir = '.';
 opendir(DIR, $dataset_dir) || die "Can't open directory $dataset_dir";
 my @models = readdir(DIR);
-my %hs_rankSVM_result;
-my %hs_rankSVM_fac_result;
+my %hs_rankSVM_test_result;
+my %hs_rankSVM_fac_test_result;
+my %hs_rankSVM_train_result;
+my %hs_rankSVM_fac_train_result;
+
+my %rankSVM_best_C;
+my %rankSVM_fac_best_C;
 my $fnOut = './result.txt';
 
 foreach  (@models){
@@ -15,17 +20,21 @@ foreach  (@models){
         #for every model we 
         if (/rankSVM/)
         {
-                my $result;
-
-
+                my $test_result;
+				my $train_result;
+				my $best_C;
                 if($_ eq 'rankSVM_fac')
                 {
-                         $result = \%hs_rankSVM_fac_result;
+                         $test_result = \%hs_rankSVM_fac_test_result;
+						 $train_result = \%hs_rankSVM_fac_train_result;
+						 $best_C = \%rankSVM_fac_best_C;
                          #print 'rankSVM_fac'."\n";
                 }
                 elsif ($_ eq 'rankSVM')
                 {
-                         $result = \%hs_rankSVM_result;
+                         $test_result = \%hs_rankSVM_test_result;
+						 $train_result = \%hs_rankSVM_train_result;
+						 $best_C = \%rankSVM_best_C;
                          #print 'rankSVM'."\n";
                 }
                 my $model_dir = $dataset_dir.'/'.$_.'/';
@@ -39,16 +48,22 @@ foreach  (@models){
                                 my $fold_dir = $model_dir.$_.'/';
                                 #print "\n".$fold_dir."\n";
                                 #(my %tmp1,my %tmp2)
-                                my  (%result_a, %result_b);
-                                my ($result_a_ref, $result_b_ref);
-                                ($result_a_ref, $result_b_ref)  =  BestVali_TestResult($fold_dir,$max_ite_num);
+                                my  (%result_a, %result_b, %result_c);
+                                my ($result_a_ref, $result_b_ref, $result_c_ref);
+                                ($result_a_ref, $result_b_ref, $result_c_ref)  =  BestVali_TestResult($fold_dir,$max_ite_num);
                                 %result_a = %$result_a_ref;
                                 %result_b = %$result_b_ref;
-                                my %hs_C_vali_map = %result_a;
+								%result_c = %$result_c_ref;
+                                my %hs_C_vali_NDCG = %result_a;								
                                 my %hs_C_test_NDCG = %result_b;
-                                my @k = sort {$hs_C_vali_map{$b} <=> $hs_C_vali_map{$a}} keys %hs_C_vali_map;
+								my %hs_C_train_NDCG = %result_c;
+								#print %hs_C_test_NDCG."\n";
+                                my @k = sort {$hs_C_vali_NDCG{$b} <=> $hs_C_vali_NDCG{$a}} keys %hs_C_vali_NDCG;
 
-                                $$result{$_} = $hs_C_test_NDCG{$k[0]};
+                                $$test_result{$_} = $hs_C_test_NDCG{$k[0]};
+								$$train_result{$_} = $hs_C_train_NDCG{$k[0]};
+								$$best_C{$_} = $k[0];
+								#print $rankSVM_best_C{$_}."\n";
                                 # print $hs_C_test_NDCG{$k[0]}."\n";
                                 # print $$result{$_}."\n";                              
                         }
@@ -57,80 +72,115 @@ foreach  (@models){
         }
 
 }
-#print %hs_rankSVM_fac_result;
-OutputResult($fnOut,\%hs_rankSVM_result,\%hs_rankSVM_fac_result);
+#print %rankSVM_best_C."\n";
+OutputResult($fnOut,\%hs_rankSVM_test_result,\%hs_rankSVM_fac_test_result,\%hs_rankSVM_train_result,\%hs_rankSVM_fac_train_result,\%rankSVM_best_C,\%rankSVM_fac_best_C);
 
 
 sub OutputResult
 {
-        my ($fnOut,$hs1,$hs2) ;
-        ($fnOut,$hs1,$hs2)= @_;
-        my %hs_rankSVM_result = %$hs1;
-        my %hs_rankSVM_fac_result = %$hs2;
-        # print %hs_rankSVM_result;
-        # print %hs_rankSVM_fac_result;
-    open(FILE, ">$fnOut");
-        print FILE "rankSVM\n";
-        print FILE "\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\n";
-        my @sum_rankSVM;
-        my @mean_rankSVM;
-        for( my$i = 1 ;$i <= 5 ; ++$i)
-        {
-                print FILE "Fold$i\t".$hs_rankSVM_result{"Fold$i"}."\n";
-                my @tmp=split(/\t/,$hs_rankSVM_result{"Fold$i"}); #@fields = split(/:/, "1:2:3:4:5");
+	my ($fnOut,$hs1,$hs2,$hs3,$hs4,$hs5,$hs6) ;
+	($fnOut,$hs1,$hs2,$hs3,$hs4,$hs5,$hs6)= @_;
+	my %hs_rankSVM_test_result = %$hs1;
+	my %hs_rankSVM_fac_test_result = %$hs2;
+	my %hs_rankSVM_train_result = %$hs3;
+	my %hs_rankSVM_fac_train_result = %$hs4;
+	my %rankSVM_best_C = %$hs5;
+	my %rankSVM_fac_best_C = %$hs6;
+	# print %hs_rankSVM_test_result;
+	# print %hs_rankSVM_fac_test_result;
+	open(FILE, ">$fnOut");
+	print FILE "rankSVM on test set"."\n";
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	my @sum_rankSVM;
+	my @mean_rankSVM;
+	for( my$i = 1 ;$i <= 5 ; ++$i)
+	{
+			print FILE "Fold$i(".$rankSVM_best_C{"Fold$i"}.")\t".$hs_rankSVM_test_result{"Fold$i"}."\n";
+			my @tmp=split(/\t/,$hs_rankSVM_test_result{"Fold$i"}); #@fields = split(/:/, "1:2:3:4:5");
 
-                for(my $j = 0;$j<$#tmp+1;$j++)
-                {
-                        $sum_rankSVM[$j]+=$tmp[$j];
-                }
-        }
+	}	
+	print FILE "\n";
+	print FILE "rankSVM on train set"."\n";	
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	for(my $i = 1; $i <= 5; $i ++)
+	{
+			print FILE "Fold$i(".$rankSVM_best_C{"Fold$i"}.")\t".($hs_rankSVM_train_result{"Fold$i"})."\n";
+			
+	}
+	print FILE "\difference between train set and test set\n";
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	for(my $i = 1; $i <= 5; $i ++)
+	{
+		my @tmp=split(/\t/,$hs_rankSVM_train_result{"Fold$i"}); #@fields = split(/:/, "1:2:3:4:5");
+		my @tmp2=split(/\t/,$hs_rankSVM_test_result{"Fold$i"});
+		my $diff;
+		print FILE "Fold$i(".$rankSVM_best_C{"Fold$i"}.")\t";
+		for(my $j = 0;$j<$#tmp+1;$j++)
+		{
+				$tmp2[$j] = $tmp[$j]-$tmp2[$j];
+				#print $tmp2[$j]."\t";
+				print FILE sprintf("%.4f\t",$tmp2[$j]);
+				#$diff = $diff.$tmp2[$j]."\t";
+		}
+		print FILE "\n";
+		#print FILE "Fold$i(".$rankSVM_fac_best_C{"Fold$i"}.")\t".$diff."\n";#$hs_rankSVM_fac_train_result{"Fold$i"}
+			
+	}
+	# print FILE "Fold1\t";#.$hs_rankSVM_result{"Fold1"};
+	print FILE "------------------------------------------"."\n";
+	my @sum_rankSVM_fac;
+	my @mean_rankSVM_fac;
+	print FILE "rankSVM_fac on test set\n";
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	for(my $i = 1; $i <= 5; $i ++)
+	{
+			print FILE "Fold$i(".$rankSVM_fac_best_C{"Fold$i"}.")\t".$hs_rankSVM_fac_test_result{"Fold$i"}."\n";
+			
+	}
+	
+	print FILE "\n";
+	print FILE "rankSVM_fac on train set\n";	
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	for(my $i = 1; $i <= 5; $i ++)
+	{		
+		print FILE "Fold$i(".$rankSVM_fac_best_C{"Fold$i"}.")\t".$hs_rankSVM_fac_train_result{"Fold$i"}."\n";#$hs_rankSVM_fac_train_result{"Fold$i"}
+			
+	}
+	print FILE "\difference between train set and test set\n";
+	print FILE "\t\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\tMeanNDCG\n";
+	for(my $i = 1; $i <= 5; $i ++)
+	{
+		my @tmp=split(/\t/,$hs_rankSVM_fac_train_result{"Fold$i"}); #@fields = split(/:/, "1:2:3:4:5");
+		my @tmp2=split(/\t/,$hs_rankSVM_fac_test_result{"Fold$i"});
+		my $diff;
+		print FILE "Fold$i(".$rankSVM_fac_best_C{"Fold$i"}.")\t";
+		for(my $j = 0;$j<$#tmp+1;$j++)
+		{
+				$tmp2[$j] = $tmp[$j]-$tmp2[$j];
+				#print $tmp2[$j]."\t";
+				print FILE sprintf("%.4f\t",$tmp2[$j]);
+				#$diff = $diff.$tmp2[$j]."\t";
+		}
+		print FILE "\n";
+			
+	}
 
-        for( my$i = 0 ;$i < 5 ; ++$i)
-        {
-                $mean_rankSVM[$i] = $sum_rankSVM[$i]/5;
-        }
-        print FILE "Mean\t";
-        for( my$i = 0 ;$i < 5 ; ++$i)
-        {
-                print FILE sprintf("%.4f\t",$mean_rankSVM[$i]);
-        }
-        print FILE "\n";
-        # print FILE "Fold1\t";#.$hs_rankSVM_result{"Fold1"};
-
-        my @sum_rankSVM_fac;
-        my @mean_rankSVM_fac;
-    print FILE "\nrankSVM_fac\n";
-        print FILE "\tMAP\tNDCG\@1\tNDCG\@3\tNDCG\@5\tNDCG\@10\n";
-        for(my $i = 1; $i <= 5; $i ++)
-        {
-                print FILE "Fold$i\t".$hs_rankSVM_fac_result{"Fold$i"}."\n";
-                my @tmp=split(/\t/,$hs_rankSVM_fac_result{"Fold$i"});
-
-                for(my $j = 0;$j<$#tmp+1;$j++)
-                {
-                        $sum_rankSVM_fac[$j]+=$tmp[$j];
-                }
-        }
-
-        for( my$i = 0 ;$i < 5 ; ++$i)
-        {
-                $mean_rankSVM_fac[$i] = $sum_rankSVM_fac[$i]/5;
-        }
-        print FILE "Mean\t";
-        for( my$i = 0 ;$i < 5 ; ++$i)
-        {
-                print FILE sprintf ("%.4f\t",$mean_rankSVM_fac[$i]);
-        }
-        print FILE "\n";
+	print FILE "\n";
 }
 
 sub BestVali_TestResult
-{
+{		#find the best performance C parameter in the validate set, and return the corresponding C value's train set
+		#and test set performance
         my $fold_dir = $_[0];
         my $max_ite_num = $_[1];
         #print $fold_dir."\n";
         my %hs_C_vali_map;
+		#the next three variables contain "map" "NDCG@1" "NDCG@3" "NDCG@5" "NDCG@10" "MeanNDCG"
+		my %hs_C_vali_NDCG;
         my %hs_C_test_NDCG;
+		my %hs_C_train_NDCG;
+		
+		my $best_C_value;
         #print $fold_dir."\n";
         opendir(FOLD_DIR, $fold_dir) || die "Can't open directory $fold_dir";
         my @C_set = readdir(FOLD_DIR);
@@ -161,7 +211,7 @@ sub BestVali_TestResult
                                                 #just find the exact vali max_ite_num file and read it
                                                 my $vali_maxIteNum_fileName = $C_dir.$_;
                                                 #$hs_C_vali_map{$C_value} = get_MAP($vali_maxIteNum_fileName);;
-												$hs_C_vali_map{$C_value} = get_MeanNDCG($vali_maxIteNum_fileName);;
+												$hs_C_vali_NDCG{$C_value} = get_MeanNDCG($vali_maxIteNum_fileName);;
                                                 # print $vali_maxIteNum_fileName."\n";
                                                 # print $hs_C_vali_map{$C_value}."\n" ;
 
@@ -176,17 +226,22 @@ sub BestVali_TestResult
                                                 $hs_C_test_NDCG{$C_value} = get_NDCG($test_maxIteNum_fileName);
                                                 #print $hs_C_test_NDCG{$C_value}."\n";
                                         }
+										elsif(/NDCG_train_ite$max_ite_num.*/)
+										{
+											my $train_maxIteNum_fileName = $C_dir.$_;
+											#print $test_maxIteNum_fileName."\n";
+											my ($tmp1,$tmp2);
+											# ($tmp1,$tmp2) = get_MAP_NDCG($test_maxIteNum_fileName);
+											$hs_C_train_NDCG{$C_value} = get_NDCG($train_maxIteNum_fileName);
+											#print $hs_C_train_NDCG{$C_value}."\n";
+										}
                                 }
                         }
                 }
         }
-
-        return (\%hs_C_vali_map,\%hs_C_test_NDCG);
-        my %tmp;
-        $tmp{'1'} = %hs_C_vali_map;
-        $tmp{'2'} = %hs_C_test_NDCG;
-        return %tmp;
-        return %hs_C_vali_map;
+		#print %hs_C_vali_NDCG."\n";
+        return (\%hs_C_vali_NDCG,\%hs_C_test_NDCG,\%hs_C_train_NDCG);      
+        
 }
 
 sub exist_MaxIteNum
@@ -234,85 +289,84 @@ sub get_NDCG
                         chomp;
                         my @tmp=split(/\t/);
                         #$vali_ite++;
-                        $mean_ndcg = (@tmp[1]+@tmp[3]+@tmp[5]+@tmp[10])/4;
-                        $set_ndcg = $map."\t".@tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t";
+                        $mean_ndcg = @tmp[11];
+                        $set_ndcg = $map."\t".@tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t".$mean_ndcg;
 
                 }
 
         }
-        close FILE;
+        close FILE;		
         return $set_ndcg;
 }
 
 sub get_MeanNDCG
 {
-        my $file_name = $_[0];
-        my $map;
-        my $set_ndcg;
-        my $mean_ndcg;
-        open(FILE, "<", $file_name) or die $!;
-        my @array=<FILE>;
-        my $index = -1;
-        foreach (@array)
-        {
-                $index++;
-                #print $index."\n";
-                if($index == 2-1)
-                {
-                        chomp;
-                        my @tmp=split(/\t/);
-                        $map = @tmp[-1];
+	my $file_name = $_[0];
+	my $map;
+	my $set_ndcg;
+	my $mean_ndcg;
+	open(FILE, "<", $file_name) or die $!;
+	my @array=<FILE>;
+	my $index = -1;
+	foreach (@array)
+	{
+			$index++;
+			#print $index."\n";
+			if($index == 2-1)
+			{
+					chomp;
+					my @tmp=split(/\t/);
+					$map = @tmp[-1];
 
-                }
-                elsif ($index == 5-1)
-                {
-                        chomp;
-                        my @tmp=split(/\t/);
-                        #$vali_ite++;
-                        #$mean_ndcg = (@tmp[1]+@tmp[3]+@tmp[5]+@tmp[10])/4;
-						$mean_ndcg = @tmp[11];
-                        $set_ndcg = @tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t";
+			}
+			elsif ($index == 5-1)
+			{
+					chomp;
+					my @tmp=split(/\t/);
+					#$vali_ite++;
+					#$mean_ndcg = (@tmp[1]+@tmp[3]+@tmp[5]+@tmp[10])/4;
+					$mean_ndcg = @tmp[11];
+					$set_ndcg = @tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t";
 
-                }
+			}
 
-        }
-        close FILE;
-        return $mean_ndcg;
+	}
+	close FILE;
+	return $mean_ndcg;
 
 }
 
 sub get_MAP
 {
-        my $file_name = $_[0];
-        my $map;
-        my $set_ndcg;
-        my $mean_ndcg;
-        open(FILE, "<", $file_name) or die $!;
-        my @array=<FILE>;
-        my $index = -1;
-        foreach (@array)
-        {
-                $index++;
-                #print $index."\n";
-                if($index == 2-1)
-                {
-                        chomp;
-                        my @tmp=split(/\t/);
-                        $map = @tmp[-1];
+	my $file_name = $_[0];
+	my $map;
+	my $set_ndcg;
+	my $mean_ndcg;
+	open(FILE, "<", $file_name) or die $!;
+	my @array=<FILE>;
+	my $index = -1;
+	foreach (@array)
+	{
+		$index++;
+		#print $index."\n";
+		if($index == 2-1)
+		{
+				chomp;
+				my @tmp=split(/\t/);
+				$map = @tmp[-1];
 
-                }
-                elsif ($index == 5-1)
-                {
-                        chomp;
-                        my @tmp=split(/\t/);
-                        #$vali_ite++;
-                        $mean_ndcg = (@tmp[1]+@tmp[3]+@tmp[5]+@tmp[10])/4;
-                        $set_ndcg = @tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t";
+		}
+		elsif ($index == 5-1)
+		{
+				chomp;
+				my @tmp=split(/\t/);
+				#$vali_ite++;
+				$mean_ndcg = @tmp[11];
+				$set_ndcg = @tmp[1]."\t".@tmp[3]."\t".@tmp[5]."\t".@tmp[10]."\t";
 
-                }
+		}
 
-        }
-        close FILE;
-        return $map;
-
+	}
+	close FILE;
+	return $map;
 }
